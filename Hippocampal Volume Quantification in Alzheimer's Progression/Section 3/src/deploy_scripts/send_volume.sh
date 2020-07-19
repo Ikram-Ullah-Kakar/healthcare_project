477721cdcd6ed0708f0d5eba269330d7e3e640fe
Ikram-Ullah-Kakar <57321896+Ikram-Ullah-Kakar@users.noreply.github.com>
2020-07-19 22:50:09 +0500

    Revert "Revert "initial comit""
    This reverts commit 66ea53b61778d7588671558610d41a54f7d05ecb.

---
+++ Hippocampal Volume Quantification in Alzheimer's Progression/Section 3/src/deploy_scripts/send_volume.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script sends a study to the Orthanc server
+
+# In your test data directory you will find three different studies - you may change the dir here
+# to try all three out
+#cp -r /data/TestVolumes /home/workspace/
+# chmod -R 777 ./
+# apt-get install dcmtk
+# deploy_scripts/send_volume.sh
+# python3 inference_dcm.py '../TestVolumes'
+
+storescu 127.0.0.1 4242 -v -aec HIPPOAI +r +sd /home/workspace/TestVolumes #/data/TestVolumes/Study1
